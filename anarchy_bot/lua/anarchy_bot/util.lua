db633c2d468b7173a17d86641d99df10d8e17f58
Zach <147861361+1054009@users.noreply.github.com>
2024-05-14 00:12:27 -0400

    Add split_message for bot_say

--- anarchy_bot/lua/anarchy_bot/util.lua
+++ anarchy_bot/lua/anarchy_bot/util.lua
@@ -19,11 +19,45 @@ function anarchy_bot.get_bot()
 	end
 end
 
+function anarchy_bot.split_message(message)
+	local splits = {}
+	local buffer = ""
+	local length = 0
+
+	if not message:EndsWith(" ") then
+		message = message .. " " -- Ensure the last word gets matched
+	end
+
+	for _, split in message:gmatch("(%s?[%S]+)") do
+		if length + split:len() >= 127 then
+			if split:StartsWith(" ") then
+				split = split:sub(2)
+			end
+
+			table.insert(splits, buffer)
+
+			buffer = split
+			length = split:len()
+		else
+			buffer = buffer .. split
+			length = length + split:len()
+		end
+	end
+
+	table.insert(splits, buffer)
+
+	return splits
+end
+
 function anarchy_bot.bot_say(message, ...)
 	libbys.arguments.validate(1, "string")
 
 	local bot = anarchy_bot.get_bot()
 	if not IsValid(bot) then return end
 
-	bot:Say(message:format(...))
+	message = message:format(...)
+
+	for _, split in ipairs(anarchy_bot.split_message(message)) do
+		bot:Say(split)
+	end
 end
