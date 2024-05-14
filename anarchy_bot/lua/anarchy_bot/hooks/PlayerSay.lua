ed0fc21ff52e8b01982e0822e64932b1ef814d08
Zach <147861361+1054009@users.noreply.github.com>
2024-05-13 23:26:46 -0400

    PlayerSay hook

---
+++ anarchy_bot/lua/anarchy_bot/hooks/PlayerSay.lua
@@ -0,0 +1,18 @@
+libbys.hook.create_unique("PlayerSay", function(ply, text, is_team_chat)
+	if not text:StartsWith("!") then return end
+
+	local args = text:sub(2):Split(" ")
+	local command_name = args[1]
+
+	for k, command in next, anarchy_bot.commands.list do
+		if k == command_name then
+			if is_team_chat then
+				anarchy_bot.bot_say("I don't run commands in team chat, %s. Stop being a bitch!", ply:GetName())
+			else
+				command:call(ply, unpack(args, 2))
+			end
+
+			break
+		end
+	end
+end)
