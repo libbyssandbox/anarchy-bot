ed5de0e7d0a2dfb45460aef11cb882df4952b628
Zach <147861361+1054009@users.noreply.github.com>
2024-05-13 21:36:13 -0400

    Use regular including

--- anarchy_bot/lua/anarchy_bot/init.lua
+++ anarchy_bot/lua/anarchy_bot/init.lua
@@ -3,10 +3,10 @@ anarchy_bot = anarchy_bot or {}
 if isfunction(anarchy_bot.kick_bot) then anarchy_bot.kick_bot("Reloading") end
 anarchy_bot.m_pBot = NULL
 
-libbys.state_include("sv_util.lua")
+include("sv_util.lua")
 
 -- Hooks
-libbys.state_include("hooks/sv_OnRequestFullUpdate.lua")
-libbys.state_include("hooks/sv_player_disconnect.lua")
+include("hooks/sv_OnRequestFullUpdate.lua")
+include("hooks/sv_player_disconnect.lua")
 
 -- Commands
