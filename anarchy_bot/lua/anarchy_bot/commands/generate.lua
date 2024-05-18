64f485ed598ea81b53f5623a05fd6c78b92e5a60
Zach <147861361+1054009@users.noreply.github.com>
2024-05-18 18:05:00 -0400

    Split !generate into separate functions

--- anarchy_bot/lua/anarchy_bot/commands/generate.lua
+++ anarchy_bot/lua/anarchy_bot/commands/generate.lua
@@ -5,7 +5,36 @@ function COMMAND:describe()
 	self:set_description("!generate (number | string) (min | length <= 100) (max | allow_symbols)")
 end
 
-function COMMAND:do_call(_, desired, min, max)
+function COMMAND.generate_number(min, max)
+	min = tonumber(min)
+	max = tonumber(max)
+
+	if not isnumber(min) or not isnumber(max) then
+		return anarchy_bot.bot_say("I need a range!")
+	end
+
+	anarchy_bot.bot_say("%i", math.random(min, max))
+end
+
+function COMMAND.generate_string(length, allow_symbols)
+	length = tonumber(length)
+
+	if isnumber(length) then
+		if length <= 100 then
+			allow_symbols = isstring(allow_symbols) and tobool(allow_symbols) or false
+
+			anarchy_bot.bot_say("\"%s\"", libbys.random.random_string(length, allow_symbols))
+		else
+			anarchy_bot.bot_say("Strings can only be 100 characters or less!")
+		end
+
+		return
+	end
+
+	anarchy_bot.bot_say("How long do you want the string?")
+end
+
+function COMMAND:do_call(_, desired, x, y)
 	if not isstring(desired) then
 		return anarchy_bot.bot_say("What do you want me to generate?")
 	end
@@ -13,28 +42,9 @@ function COMMAND:do_call(_, desired, min, max)
 	desired = desired:lower()
 
 	if desired == "number" then
-		min = tonumber(min)
-		max = tonumber(max)
-
-		if not isnumber(min) or not isnumber(max) then
-			return anarchy_bot.bot_say("I need a range!")
-		end
-
-		anarchy_bot.bot_say("%i", math.random(min, max))
+		self.generate_number(x, y)
 	elseif desired == "string" then
-		local length = tonumber(min)
-
-		if isnumber(length) then
-			if length <= 100 then
-				local allow_symbols = isstring(max) and tobool(max) or false
-
-				anarchy_bot.bot_say("\"%s\"", libbys.random.random_string(length, allow_symbols))
-			else
-				anarchy_bot.bot_say("Strings can only be 100 characters or less!")
-			end
-		else
-			anarchy_bot.bot_say("How long do you want the string?")
-		end
+		self.generate_string(x, y)
 	else
 		anarchy_bot.bot_say("What the FUCK is a %s", desired)
 	end
