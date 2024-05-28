6b46dd283beeef25e3a98c0aa903f00c9516a02d
Zach <147861361+1054009@users.noreply.github.com>
2024-05-28 09:51:37 -0400

    Let's try that again

--- anarchy_bot/lua/anarchy_bot/hooks/OnRequestFullUpdate.lua
+++
@@ -1,8 +0,0 @@
-anarchy_bot.connect_cache = {}
-
-libbys.hook.create_unique("OnRequestFullUpdate", function(data)
-	if data.userid == "BOT" then return end
-	if anarchy_bot.connect_cache[data.userid] then return end
-
-	anarchy_bot.ensure_bot() -- Ensure the bot is valid
-end)
--- anarchy_bot/lua/anarchy_bot/hooks/player_disconnect.lua
+++
@@ -1,8 +0,0 @@
-libbys.hook.create_unique("player_disconnect", function(data)
-	anarchy_bot.connect_cache[data.userid] = nil
-
-	-- Everyone left
-	if player.GetCount() - 1 <= 0 then
-		anarchy_bot.kick_bot("Punting bot, server is hibernating")
-	end
-end)
