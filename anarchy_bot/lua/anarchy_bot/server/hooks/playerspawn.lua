57f611aa5bd7cc252ad1ed9dd7033ffb36d6996e
Zach <147861361+1054009@users.noreply.github.com>
2024-05-28 11:57:12 -0400

    Comment

--- anarchy_bot/lua/anarchy_bot/server/hooks/playerspawn.lua
+++ anarchy_bot/lua/anarchy_bot/server/hooks/playerspawn.lua
@@ -1,6 +1,7 @@
 hook.Add("PlayerSpawn", "anarchy_bot", function(ply)
 	if ply ~= anarchy_bot:get_bot() then return end
 
+	-- Spawn facing a random direction
 	local face_angles = ply:EyeAngles()
 	face_angles.yaw = math.Rand(-180, 180)
 
