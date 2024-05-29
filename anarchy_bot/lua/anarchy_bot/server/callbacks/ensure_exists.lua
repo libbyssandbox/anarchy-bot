a44472b1e9536a5ee14618edb12b707bbc91425a
Zach <147861361+1054009@users.noreply.github.com>
2024-05-29 01:12:36 -0400

    Rejoin if disconnected

--- anarchy_bot/lua/anarchy_bot/server/callbacks/ensure_exists.lua
+++ anarchy_bot/lua/anarchy_bot/server/callbacks/ensure_exists.lua
@@ -3,3 +3,11 @@ anarchy_bot:add_callback("PlayerInitialSpawn", function(ply)
 
 	anarchy_bot:ensure_bot()
 end)
+
+anarchy_bot:add_callback("player_disconnect", function(data)
+	if data.bot ~= 1 then return end
+
+	timer.Simple(0, function()
+		anarchy_bot:ensure_bot()
+	end)
+end)
