c86aea70b8c07fa5b06d420293f6c1362b8c2da3
Zach <147861361+1054009@users.noreply.github.com>
2024-05-29 00:59:59 -0400

    help command
    Needs trimming in the future

---
+++ anarchy_bot/lua/anarchy_bot/server/commands/help.lua
@@ -0,0 +1,36 @@
+local command = {}
+
+function command:describe()
+	self:set_description("!help | !help (command_name)")
+end
+
+function command:help_with(found_command)
+	if not found_command then
+		anarchy_bot:say("I can't find that command!")
+		return true
+	end
+
+	anarchy_bot:say("!%s - %s", found_command:get_name(), found_command:get_description())
+end
+
+function command:help_all()
+	local generated = {}
+
+	for index_name, index_command in SortedPairs(anarchy_bot.commands) do
+		if index_name == "_BASE" or index_name == "_BASE_CONFIG" then continue end
+
+		table.insert(generated, ("!%s"):format(index_command:get_name()))
+	end
+
+	anarchy_bot:say(table.concat(generated, ", "))
+end
+
+function command:do_call(bot, ply, command_name)
+	if isstring(command_name) then
+		return self:help_with(anarchy_bot.commands[command_name:lower()])
+	else
+		return self:help_all()
+	end
+end
+
+anarchy_bot:register_command(command, "help")
