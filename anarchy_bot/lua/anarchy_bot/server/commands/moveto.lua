815884e8c7220c8391be524f8f3195c16e288530
Zach <147861361+1054009@users.noreply.github.com>
2024-06-01 05:38:25 -0400

    Use utils for teleporting

--- anarchy_bot/lua/anarchy_bot/server/commands/moveto.lua
+++ anarchy_bot/lua/anarchy_bot/server/commands/moveto.lua
@@ -1,34 +1,9 @@
 local command = {}
 
 function command:describe()
-	self.m_TraceResult = {}
-	self.m_TraceData = { output = self.m_TraceResult }
-
 	self:set_description("!moveto (target | x) (y) (z)")
 
 	self:get_config():set_cooldown_duration(10)
-end
-
-function command:run_trace(start_pos, end_pos, mins, maxs, ignore)
-	self.m_TraceData.start = start_pos
-	self.m_TraceData.endpos = end_pos
-	self.m_TraceData.mins = mins
-	self.m_TraceData.maxs = maxs
-	self.m_TraceData.filter = ignore
-
-	util.TraceHull(self.m_TraceData)
-
-	return self.m_TraceResult
-end
-
-function command:try_teleport(ply, position)
-	local tr = self:run_trace(position, position, ply:OBBMins(), ply:OBBMaxs(), ply)
-	if tr.Hit then return false end
-
-	ply:SetPos(position)
-	ply:SetVelocity(-ply:GetVelocity())
-
-	return true
 end
 
 function command:do_call(bot, ply, x, y, z)
@@ -38,42 +13,15 @@ function command:do_call(bot, ply, x, y, z)
 	end
 
 	-- Try x y z
-	if isnumber(tonumber(x)) and isnumber(tonumber(y)) and isnumber(tonumber(z)) then
-		local position = Vector(x, y, z)
-
-		if self:try_teleport(ply, position) then
-			anarchy_bot:say("Here we go!")
-			return
-		end
-
-		anarchy_bot:say("Your fatass doesn't fit!")
-		return true
+	if libbys.util.try_teleport(ply, x, y, z) then
+		anarchy_bot:say("Here we go!")
+		return
 	end
 
 	-- Try a player
-	local target = libbys.util.find_player(x)
-
-	if IsValid(target) then
-		local target_origin = target:GetPos()
-		local target_radius = target:BoundingRadius()
-
-		-- 4 cardinal directions
-		local positions = {
-			Vector(target_origin.x + target_radius, target_origin.y, target_origin.z),
-			Vector(target_origin.x - target_radius, target_origin.y, target_origin.z),
-			Vector(target_origin.x, target_origin.y + target_radius, target_origin.z),
-			Vector(target_origin.x, target_origin.y - target_radius, target_origin.z)
-		}
-
-		for i = 1, #positions do
-			if self:try_teleport(ply, positions[i]) then
-				anarchy_bot:say("Here we go!")
-				return
-			end
-		end
-
-		anarchy_bot:say("Your fatass doesn't fit!")
-		return true
+	if libbys.util.try_teleport(ply, libbys.util.find_player(x)) then
+		anarchy_bot:say("Here we go!")
+		return
 	end
 
 	-- Idiot
